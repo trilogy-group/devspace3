23abbd6a4e1603fbf1be834a5d41203ed4e082b1
Vladimir Pikalov <vladimir.pikalov@aurea.com>
2020-11-01 10:39:55 +0700

    base

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace devspace2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Test2");
+        }
+    }
+}
